08b30de301f9e0f830649c7ab5eced3e533ad7f0
benji7425 <benji7425@outlook.com>
2017-08-09 20:24:21 +0100

    Add some more event/error logging

--- wrapper.js
+++ wrapper.js
@@ -4,16 +4,17 @@ const DiscordUtil = require("discordjs-util");
 const client = new Discord.Client();
 
 process.on("uncaughtException", (err) => {
-	DiscordUtil.dateError(err);
+	DiscordUtil.dateError("Uncaught exception!", err);
 });
 
 client.login(require("./token.json").token);
 
 client.on("ready", () => {
+	DiscordUtil.dateLog("Registered bot " + client.user.username);
 	require("./app/index.js")(client);
 	client.user.setGame("benji7425.github.io");
 });
 
 client.on("disconnect", eventData => {
-	DiscordUtil.dateError(eventData.code, eventData.reason);
+	DiscordUtil.dateError("Bot was disconnected!", eventData.code, eventData.reason);
 });
