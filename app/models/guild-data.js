38dcf45b62598d53528d91ee5e5096d2c3fe92b3
benji7425 <benji7425@outlook.com>
2018-02-08 21:31:28 +0000

    Add setup step for configuring inactive role

--- app/models/guild-data.js
+++ app/models/guild-data.js
@@ -8,19 +8,11 @@ module.exports = class GuildData extends Core.BaseGuildData {
 
         this.inactiveThresholdDays = { type: Number, default: 7, min: 1 };
         this.activeRoleID = String;
+        this.inactiveRoleID = String;
         this.users = { type: Object, default: {} };
         this.allowRoleAddition = Boolean;
         this.ignoredUserIDs = [String];
         this.ignoredRoleIDs = [String];
-
-        // this.schema({
-        // 	inactiveThresholdDays: Number,
-        // 	activeRoleID: String,
-        // 	users: this.users, //bit of a hack
-        // 	allowRoleAddition: Boolean,
-        // 	ignoredUserIDs: [String],
-        // 	ignoredRoleIDs: [String]
-        // });
     }
 
     checkUsers(client) {
