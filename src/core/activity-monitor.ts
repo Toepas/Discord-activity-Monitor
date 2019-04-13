11a8bac8aed8b3cfcda00f9c79ad7c84c885a443
Benji Higgins <benji7425@pm.me>
2019-04-13 18:35:53 +0100

    Add marking users active on message or voice state change
    st

---
+++ src/core/activity-monitor.ts
@@ -0,0 +1,64 @@
+import { Client, BotGuildMember, Logger } from "disharmony";
+import Message from "../models/message";
+import Guild from "../models/guild";
+
+export default class ActivityMonitor
+{
+    private async registerActivity(guild: Guild, member: BotGuildMember)
+    {
+        await guild.loadDocument()
+        if (guild
+            && member && member.id != this.client.botId
+            && this.isGuildSetUp(guild))
+        {
+            guild.users.set(member.id, new Date())
+            await this.markActiveIfNotIgnored(guild, member, true)
+            guild.save()
+        }
+    }
+
+    private async markActiveIfNotIgnored(guild: Guild, member: BotGuildMember, isActive: boolean)
+    {
+        const addRole = isActive ? guild.activeRoleId : guild.inactiveRoleId
+        const removeRole = isActive ? guild.inactiveRoleId : guild.activeRoleId
+
+        try
+        {
+            if (this.isMemberIgnored(guild, member))
+                return
+
+            if (addRole && addRole !== "disabled")
+                await member.addRole(addRole);
+
+            if (removeRole && removeRole !== "disabled")
+                await member.removeRole(removeRole)
+        }
+        catch (e)
+        {
+            Logger.consoleLog(
+                `Error marking user ${member.username} ${isActive ? "active" : "inactive"} in guild ${guild.name}.
+                    ${e.message || e}`,
+                true)
+        }
+    }
+
+    private isMemberIgnored(guild: Guild, member: BotGuildMember)
+    {
+        const isIgnoredUser = guild.ignoredUserIds.indexOf(member.id) >= 0
+        const hasIgnoredRole = guild.ignoredRoleIds.some(roleId => member.hasRole(roleId))
+        return isIgnoredUser || hasIgnoredRole
+    }
+
+    private isGuildSetUp(guild: Guild)
+    {
+        return guild.allowRoleAddition && guild.activeRoleId && guild.activeRoleId.length > 0
+    }
+
+    constructor(
+        private client: Client<Message>
+    )
+    {
+        this.client.onMessage.sub(message => this.registerActivity(message.guild, message.member))
+        this.client.onVoiceStateUpdate.sub(member => this.registerActivity(new Guild(member.djsGuild), member))
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Client } from "disharmony"
 import Message from "./models/message";
+import ActivityMonitor from "./core/activity-monitor";
 
-let client = new Client("Role Assigner", require("./commands"), Message)
+const client = new Client("Role Assigner", require("./commands"), Message)
+const activityMonitor = new ActivityMonitor(client)
 
 client.initialize(require("fs").readFileSync("./token", "utf8"))
