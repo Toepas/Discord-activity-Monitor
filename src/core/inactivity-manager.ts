677984a534e237caa49a90d5302f3ed7cb65ccf6
Benji Higgins <benji7425@pm.me>
2019-04-16 00:43:29 +0100

    Add worker to iterate guild members and mark inactive users
    Currently hard coded to run every 24 hours

--- src/core/inactivity-manager.ts
+++ src/core/inactivity-manager.ts
@@ -1,6 +1,7 @@
 import { initialize as initializeDb } from "disharmony/dist/database/db-client"
 import { Client as DjsClient } from "discord.js"
 import Guild from "../models/guild";
+import { Logger } from "disharmony";
 
 export default class InactivityManager
 {
@@ -22,7 +23,15 @@ export default class InactivityManager
         await this.djsClient.destroy()
     }
 
-    public async manageInactiveUsers(guildId: string)
+    public async manageInactiveUsersInAllGuilds()
+    {
+        Logger.debugLog("Beginning guild iteration to manage inactive users")
+        for (let guild of this.djsClient.guilds.values())
+            await this.manageInactiveUsersInGuild(guild.id)
+        Logger.debugLog("Guilds iteration complete")
+    }
+
+    public async manageInactiveUsersInGuild(guildId: string)
     {
         const djsGuild = this.djsClient.guilds.get(guildId)
         if (!djsGuild)
@@ -34,6 +43,42 @@ export default class InactivityManager
         if (!guild.activeRole)
             return
 
-        await guild.activeRole.members.get("117966411548196870")!.removeRole(guild.activeRoleId)
+        Logger.debugLog(`Managing inactives for guild ${guild.name}`)
+        for (let member of guild.activeRole.members.values())
+        {
+            //don't ask me why, sometimes member is null
+            if (!member)
+                return
+
+            const now = new Date()
+            if (!guild.users.get(member.id))
+            {
+                guild.users.set(member.id, now) //if a user has the active role but isn't in the database, add them
+                Logger.debugLog(`User ${member.user.username} has active role but not found in database, adding new entry`)
+            }
+            else if (this.isInactiveBeyondThreshold(guild.users.get(member.id)!, now, guild.inactiveThresholdDays))
+                {
+                    try
+                    {
+                        await member.removeRole(guild.activeRole)
+                        if (guild.inactiveRoleId && guild.inactiveRoleId !== "disabled")
+                            await member.addRole(guild.inactiveRoleId)
+                        guild.users.delete(member.id)
+                        Logger.debugLog(`Updated now inactive user ${member.user.username}`)
+                    }
+                    catch (e)
+                    {
+                        Logger.consoleLog(`Error switching user ${member.user.username} to inactive in guild ${guild.name}`, true)
+                    }
+                }
+            }
+
+            await guild.save()
+        }
+
+    private isInactiveBeyondThreshold(lastActiveDate: Date, now: Date, thresholdDays: number): boolean
+    {
+        const dayLength = 24 * 60 * 60 * 1000
+        return Math.round(Math.abs(now.getTime() - lastActiveDate.getTime()) / dayLength) > thresholdDays
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,47 @@
-import { Client } from "disharmony"
+import { Client, Logger } from "disharmony"
 import Message from "./models/message";
 import * as Cluster from "cluster"
 import ActivityRegisterer from "./core/activity-registerer";
 import InactivityManager from "./core/inactivity-manager";
+
+const token = require("fs").readFileSync("./token", "utf8")
 
 if (Cluster.isMaster)
 {
     const client = new Client("Role Assigner", require("./commands"), Message)
     new ActivityRegisterer(client).startListening()
 
-    client.initialize(require("fs").readFileSync("./token", "utf8"))
+    client.initialize(token)
 
-    const worker = Cluster.fork()
-
-    process.on("SIGINT", () => worker.kill())
-    process.on("exit", () => worker.kill())
+    setTimeout(spawnInactiveUserManagementWorker, 24 * 60 * 60 * 1000)
 }
 else
 {
-    forked()
+    manageInactiveMembersInAllGuilds()
 }
 
-async function forked()
+async function manageInactiveMembersInAllGuilds()
 {
-    console.log("Hello!")
     const inactivityManager = new InactivityManager()
-    await inactivityManager.connect(require("fs").readFileSync("./token", "utf8"))
-    await inactivityManager.manageInactiveUsers("264420391282409473")
+    await inactivityManager.connect(token)
+    await inactivityManager.manageInactiveUsersInAllGuilds()
+    await inactivityManager.disconnect();
+    process.exit(0)
+}
+
+function spawnInactiveUserManagementWorker()
+{
+    const worker = Cluster.fork()
+    Logger.debugLog(`Spawned process PID ${worker.process.pid} to manage inactive users`)
+    const killWorker = () => worker.kill()
+
+    process.on("SIGINT", killWorker)
+    process.on("exit", killWorker)
+
+    worker.on("exit", () =>
+    {
+        Logger.debugLog(`Process ${worker.process.pid} exited`)
+        process.removeListener("SIGINT", killWorker)
+        process.removeListener("exit", killWorker)
+    })
 }
