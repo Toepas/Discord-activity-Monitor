3907b7d5647d9684291c69237fe81f034f55afef
benji7425 <benji7425@pm.me>
2019-06-02 11:56:11 +0100

    Fix ignored user and role ids not saving after first document create

--- src/models/guild.ts
+++ src/models/guild.ts
@@ -4,42 +4,31 @@ export default class Guild extends BotGuild
 {
     private _users: Map<string, Date>
 
-    get inactiveThresholdDays(): number { return this.record.inactiveThresholdDays || 7 }
-    set inactiveThresholdDays(value: number) { this.record.inactiveThresholdDays = value }
+    public get inactiveThresholdDays(): number { return this.record.inactiveThresholdDays || 7 }
+    public set inactiveThresholdDays(value: number) { this.record.inactiveThresholdDays = value }
 
-    get activeRoleId(): string { return this.record.activeRoleId || "" }
-    set activeRoleId(value: string) { this.record.activeRoleId = value }
+    public get activeRoleId(): string { return this.record.activeRoleId || "" }
+    public set activeRoleId(value: string) { this.record.activeRoleId = value }
 
-    get activeRole() { return this.djs.roles.get(this.activeRoleId) }
+    public get activeRole() { return this.djs.roles.get(this.activeRoleId) }
 
-    get inactiveRoleId(): string { return this.record.inactiveRoleId || "" }
-    set inactiveRoleId(value: string) { this.record.inactiveRoleId = value }
+    public get inactiveRoleId(): string { return this.record.inactiveRoleId || "" }
+    public set inactiveRoleId(value: string) { this.record.inactiveRoleId = value }
 
-    get allowRoleAddition(): boolean { return this.record.allowRoleAddition }
-    set allowRoleAddition(value: boolean) { this.record.allowRoleAddition = value }
+    public get allowRoleAddition(): boolean { return this.record.allowRoleAddition }
+    public set allowRoleAddition(value: boolean) { this.record.allowRoleAddition = value }
 
-    get ignoredUserIds(): string[]
-    {
-        if (!this.record.ignoredUserIds)
-            this.record.ignoredUserIds = []
-        return this.record.ignoredUserIds
-    }
-
-    get ignoredRoleIds(): string[]
-    {
-        if (!this.record.ignoredRoleIds)
-            this.record.ignoredRoleIds = []
-        return this.record.ignoredRoleIds
-    }
-
-    get users(): Map<string, Date>
+    public get users(): Map<string, Date>
     {
         if (!this._users)
             this._users = new Map(this.record.users || [])
         return this._users
     }
 
-    public configJson()
+    public ignoredUserIds: string[]
+    public ignoredRoleIds: string[]
+
+    public getConfigJson()
     {
         const blacklist = ["id", "_id", "users"]
         return JSON.stringify(
@@ -49,8 +38,17 @@ export default class Guild extends BotGuild
         )
     }
 
+    public loadRecord(record: any)
+    {
+        this.ignoredUserIds = record.ignoredUserIds || []
+        this.ignoredRoleIds = record.ignoredRoleIds || []
+        return super.loadRecord(record)
+    }
+
     public toRecord()
     {
+        this.record.ignoredUserIds = this.ignoredUserIds
+        this.record.ignoredRoleIds = this.ignoredRoleIds
         this.record.users = this._users ? [...this._users.entries()] : []
         return super.toRecord()
     }
