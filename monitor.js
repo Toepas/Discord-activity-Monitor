089b8860eb44d0e4dbdbaf2f563b04d1221afdc1
benji7425 <benji7425@pm.me>
2019-10-31 13:26:39 +0000

    Fix a few exiting issues with monitor.js
    It wouldn't always exit properly when expected

--- monitor.js
+++ monitor.js
@@ -5,7 +5,8 @@
 const forever = require("forever-monitor")
 
 let restartInterval = null
-let isRestartingOrExiting = false
+let isShuttingDown = false
+let isPerformingScheduledRestart = false
 
 function logStatement(statement)
 {
@@ -20,36 +21,44 @@ const child = new (forever.Monitor)("./dist/index.js", {
 });
 child.on("exit", () =>
 {
-    if (!isRestartingOrExiting)
+    if (!isShuttingDown && !isPerformingScheduledRestart)
+    {
         logStatement("Application has exited unexpectedly, this is likely due to a repeat failure")
+        if (restartInterval)
+            clearInterval(restartInterval)
+    }
 });
 
 // Attach handlers to clean up child and interval before exit
-function cleanUpBeforeExit()
+function shutDown()
 {
     try
     {
         if (restartInterval)
             clearInterval(restartInterval)
-        isRestartingOrExiting = true
-        child.kill(true)
-        logStatement("Application has gracefully shut down")
+
+        if (!isShuttingDown)
+        {
+            isShuttingDown = true
+            child.stop()
+            child.kill(true).once("exit", () => logStatement("Application has gracefully shut down"))
+        }
     }
     catch{ }
 }
 
-process.on("SIGINT", cleanUpBeforeExit)
-process.on("SIGTERM", cleanUpBeforeExit)
+process.on("SIGINT", shutDown)
+process.on("SIGTERM", shutDown)
 
 // Set the interval for restarting the application
 async function restartApplication()
 {
-    isRestartingOrExiting = true
+    isPerformingScheduledRestart = true
     child.once("exit", () => logStatement("Application is performing scheduled restart..."))
     child.kill(true) // Kill the child
     await new Promise(resolve => setTimeout(resolve, 5000)) // Wait 5s
     child.start() // Recreate the child
-    isRestartingOrExiting = false
+    isPerformingScheduledRestart = false
 }
 
 restartInterval = setInterval(restartApplication, 24 * 60 * 60 * 1000)
